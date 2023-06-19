2bf6c3184bd0161cf892bb07bd267d780d76f0b7
felpb <fallensky510@gmail.com>
2023-06-18 23:52:37 -0300

    feat: cards created by js

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let moviesList = [{"id":1,
   "name":"test",
   "image":"test",
-  "description":"test"}
+  "description":"test"},
 ];
 
 moviesList.map((retorno) => {
-  console.log(retorno.name)
+  let movies = document.querySelector("#cards");
+  movies.innerHTML += `
+    <div class="col-md-4">
+      <div class="card m-2">
+        <img src="..."  class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  `;
 });
 
 
