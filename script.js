922c6803813a1bb668300c3d918e54c437095c78
felpb <fallensky510@gmail.com>
2023-06-19 22:16:17 -0300

    feat: modal card

--- script.js
+++ script.js
@@ -10,19 +10,29 @@ let moviesList = [{"id":1,
 },
 ];
 
-moviesList.map((retorno) => {
+moviesList.map((card, posicao) => {
   let movies = document.querySelector("#cards");
   movies.innerHTML += `
     <div class="col-md-4">
       <div class="card m-2">
-        <img src="${retorno.image}"  class="card-img-top" alt="...">
+        <img src="${card.image}"  class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${retorno.name}</h5>
+          <h5 class="card-title">${card.name}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a href="#" class="btn btn-primary" onclick="openCard(${posicao})">Go somewhere</a>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
     </div>
   `;
 });
+
+function openCard(posicao) {
+  let selectCard = moviesList[posicao];
+  
+  document.querySelector('#titleModal').innerHTML = selectCard.name;
+  document.querySelector('#imgModal').src = selectCard.image;
+  document.querySelector('#obsModal').innerHTML = selectCard.description;
+
+  new bootstrap.Modal('#openCard').show();
+}
